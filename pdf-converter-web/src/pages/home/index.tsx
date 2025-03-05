57570763eb08ada1583abf05d6439fdd0a97342d
Andreyna Carvalho <andreyna.m.carvalho@gmail.com>
2025-03-04 22:48:40 -0300

    [FRONT] Updated Home

--- pdf-converter-web/src/pages/home/index.tsx
+++ pdf-converter-web/src/pages/home/index.tsx
@@ -11,8 +11,13 @@ const Home = () => {
     <Layout>
       <Helmet>
         <meta
-          name="description"
-          content="Sobre Andreyna Carvalho e o motivo do desenvolvimento do projeto."
+          name="home"
+          content="Página inicial com todos os serviços disponiveis, 
+          Merge PDF, Mesclar múltiplos PDFs em um único arquivo, Remove Pages, 
+          Remover páginas específicas de um PDF, Compress PDF, Reduzir o tamanho do arquivo PDF, 
+          PDF to JPG, Converter um PDF para imagens JPG, PDF to WORD, Converter um PDF para documento 
+          editável do Word, JPG to PDF, Converter imagens JPG para PDF, Formatar word padrão ABNT, 
+          Classificação de provas"
         />
         <title>Página inicial</title>
       </Helmet>
@@ -25,7 +30,7 @@ const Home = () => {
             <Elements.Service
               key={service.name}
               className="service-item"
-              onClick={() => navigate(`/${service.name}`)}
+              onClick={() => navigate(`/${service.urlReq}`)}
             >
               <Elements.NameService>{service.name}</Elements.NameService>
               <Elements.BodyService>{service.description}</Elements.BodyService>
--- pdf-converter-web/src/routes/index.tsx
+++ pdf-converter-web/src/routes/index.tsx
@@ -1,11 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import About from "../pages/about";
+import Home from "../pages/home";
+import Converters from "../components/converters";
 
 export const Router = () => {
   return (
     <Routes>
-      {/* <Route path="/" element={<Layout />}></Route> */}
+      <Route path="/" element={<Home />}></Route>
       <Route path="/about" element={<About />}></Route>
+      <Route path="converter/*" element={<Converters />}></Route>
+      <Route path="pdf/*" element={<Converters />}></Route>
+      <Route path="format/abnt" element={<Home />}></Route>
+      <Route path="classification/get-result" element={<Home />}></Route>
     </Routes>
   );
 };
